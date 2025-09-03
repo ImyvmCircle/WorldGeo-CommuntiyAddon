51f17193b42d6a939a70357af10724b6cce800db
Doohaey Re <doohaey@gmail.com>
2025-09-04 00:04:46 +0800

    chore: import of world-geo.

--- src/main/kotlin/com/imyvm/community/domain/Community.kt
+++ src/main/kotlin/com/imyvm/community/domain/Community.kt
@@ -1,6 +1,9 @@
 package com.imyvm.community.domain
 
+import com.imyvm.iwg.domain.Region
+
 class Community {
     val name: String = "community"
     val id: Int = 0
+    val region: Region? = null
 }
