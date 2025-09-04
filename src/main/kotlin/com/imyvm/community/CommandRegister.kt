5376df30ddff2b6f638dda580f9bc723dcb33d81
Doohaey Re <doohaey@gmail.com>
2025-09-04 16:25:27 +0800

    feat: api call trial.

--- src/main/kotlin/com/imyvm/community/CommandRegister.kt
+++ src/main/kotlin/com/imyvm/community/CommandRegister.kt
@@ -1,10 +1,26 @@
 package com.imyvm.community
 
+import com.imyvm.iwg.application.resetSelection
+import com.imyvm.iwg.application.startSelection
+import com.imyvm.iwg.application.stopSelection
+import com.imyvm.iwg.domain.Region
 import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.suggestion.SuggestionProvider
 import net.minecraft.command.CommandRegistryAccess
+import net.minecraft.server.command.CommandManager.argument
 import net.minecraft.server.command.CommandManager.literal
 import net.minecraft.server.command.ServerCommandSource
+import java.util.*
+import java.util.concurrent.CompletableFuture
+
+private val SHAPE_TYPE_SUGGESTION_PROVIDER: SuggestionProvider<ServerCommandSource> = SuggestionProvider { _, builder ->
+    Region.Companion.GeoShapeType.entries
+        .filter { it != Region.Companion.GeoShapeType.UNKNOWN }
+        .forEach { builder.suggest(it.name.lowercase(Locale.getDefault())) }
+    CompletableFuture.completedFuture(builder.build())
+}
 
 fun register(dispatcher: CommandDispatcher<ServerCommandSource>, registryAccess: CommandRegistryAccess) {
     dispatcher.register(
@@ -13,10 +29,56 @@ fun register(dispatcher: CommandDispatcher<ServerCommandSource>, registryAccess:
                 literal("help")
                     .executes{ runHelpCommand(it) }
             )
-
+            .then(
+                literal("select")
+                    .then(
+                        literal("start")
+                            .executes { runStartSelect(it) }
+                    )
+                    .then(
+                        literal("stop")
+                            .executes { runStopSelect(it) }
+                    )
+                    .then(
+                        literal("reset")
+                            .executes { runResetSelect(it) }
+                    )
+            )
+            .then(
+                literal("create")
+                    .then(
+                        argument("shapeType", StringArgumentType.word())
+                            .suggests(SHAPE_TYPE_SUGGESTION_PROVIDER)
+                            .then(
+                                argument("name", StringArgumentType.word())
+                                    .executes { runCreateCommunity(it) }
+                            )
+                    )
+            )
     )
 }
 
 private fun runHelpCommand(context: CommandContext<ServerCommandSource>): Int {
+    TODO()
+}
+
+private fun runStartSelect(context: CommandContext<ServerCommandSource>): Int {
+    val player = context.source.player ?: return 0
+    return startSelection(player)
+}
+
+private fun runStopSelect(context: CommandContext<ServerCommandSource>): Int {
+    val player = context.source.player ?: return 0
+    return stopSelection(player)
+}
+
+private fun runResetSelect(context: CommandContext<ServerCommandSource>): Int {
+    val player = context.source.player ?: return 0
+    return resetSelection(player)
+}
+
+private fun runCreateCommunity(context: CommandContext<ServerCommandSource>): Int {
+    val name = StringArgumentType.getString(context, "name")
+
     TODO()
 }
