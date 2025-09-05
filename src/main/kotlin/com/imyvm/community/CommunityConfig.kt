8636e3a49780be3b23b43d093b8afdc37723b3f9
Doohaey Re <doohaey@gmail.com>
2025-09-05 16:29:27 +0800

    chore: add reserved configurations for more automatic community creation check.

--- src/main/kotlin/com/imyvm/community/CommunityConfig.kt
+++ src/main/kotlin/com/imyvm/community/CommunityConfig.kt
@@ -25,5 +25,35 @@ class CommunityConfig : HokiConfig("Community.conf") {
         ) { obj, path ->
             obj.getLong(path)
         }
+
+        @JvmField
+        @ConfigOption
+        val IS_CHECKING_REGION_AREA = Option(
+            "region.is_checking_area",
+            true,
+            "whether to check the area of the selected region when creating a community."
+        ) { obj, path ->
+            obj.getBoolean(path)
+        }
+
+        @JvmField
+        @ConfigOption
+        val MAX_MANOR_AREA = Option(
+            "region.max_manor_area",
+            50000,
+            "the maximum area of a manor region."
+        ) { obj, path ->
+            obj.getInt(path)
+        }
+
+        @JvmField
+        @ConfigOption
+        val IS_CHECKING_DEVELOPMENT = Option(
+            "development.is_checking",
+            false,
+            "whether to enable development mode."
+        ) { obj, path ->
+            obj.getBoolean(path)
+        }
     }
 }
