ce923ddfbf88b9f4d92baee0936e3ee406f63063
Doohaey Re <doohaey@gmail.com>
2025-10-09 00:20:52 +0800

    fix: provider and mode changer for new filter mode.

--- src/main/kotlin/com/imyvm/community/application/interaction/screen/CommunityListMenuHandler.kt
+++ src/main/kotlin/com/imyvm/community/application/interaction/screen/CommunityListMenuHandler.kt
@@ -6,9 +6,11 @@ import net.minecraft.server.network.ServerPlayerEntity
 fun runSwitchMode(player: ServerPlayerEntity, cont: Any?){
     when (cont as? String) {
         "ALL" -> CommunityMenuOpener.open(player, "ALL") { syncId, content -> CommunityListMenu(syncId, content) }
+        "JOIN-ABLE" -> CommunityMenuOpener.open(player, "JOIN-ABLE") { syncId, content -> CommunityListMenu(syncId, content) }
         "RECRUITING" -> CommunityMenuOpener.open(player, "RECRUITING") { syncId, content -> CommunityListMenu(syncId, content) }
         "AUDITING" -> CommunityMenuOpener.open(player, "AUDITING") { syncId, content -> CommunityListMenu(syncId, content) }
         "ACTIVE" -> CommunityMenuOpener.open(player, "ACTIVE") { syncId, content -> CommunityListMenu(syncId, content) }
+        "REVOKED" -> CommunityMenuOpener.open(player, "REVOKED") { syncId, content -> CommunityListMenu(syncId, content) }
         else -> CommunityMenuOpener.open(player, "ALL") { syncId, content -> CommunityListMenu(syncId, content) }
     }
 }
