eb777b9bd6b4ecf97c27520c6001e3c8af522834
Doohaey Re <doohaey@gmail.com>
2025-10-16 11:48:10 +0800

    fix: now slots in anvil(creation rename) are read-only.

--- src/main/kotlin/com/imyvm/community/application/interaction/screen/CommunityCreationRenameHandler.kt
+++ src/main/kotlin/com/imyvm/community/application/interaction/screen/CommunityCreationRenameHandler.kt
@@ -1,17 +1,20 @@
 package com.imyvm.community.application.interaction.screen
 
 import com.imyvm.community.inter.screen.CommunityCreationMenu
+import com.imyvm.community.inter.screen.component.ReadOnlySlot
 import com.imyvm.community.util.Translator
 import com.imyvm.iwg.domain.Region
 import net.minecraft.component.DataComponentTypes
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.inventory.SimpleInventory
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
 import net.minecraft.screen.AnvilScreenHandler
 import net.minecraft.screen.NamedScreenHandlerFactory
 import net.minecraft.screen.ScreenHandler
 import net.minecraft.screen.ScreenHandlerContext
+import net.minecraft.screen.slot.SlotActionType
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
 
@@ -30,14 +33,25 @@ object CommunityCreationRenameHandler {
             override fun createMenu(syncId: Int, inv: PlayerInventory, p: PlayerEntity): ScreenHandler {
                 val context = ScreenHandlerContext.create(p.world, p.blockPos)
 
+                val simpleInventory = SimpleInventory(3)
                 val anvil = object : AnvilScreenHandler(syncId, inv, context) {
+
+                    init {
+                        this.slots[INPUT_1_ID] = ReadOnlySlot(simpleInventory, INPUT_1_ID, 27, 47)
+                        this.slots[INPUT_2_ID] = ReadOnlySlot(simpleInventory, INPUT_2_ID, 76, 47)
+                        this.slots[OUTPUT_ID] = ReadOnlySlot(simpleInventory, OUTPUT_ID, 125, 47)
+                    }
 
                     override fun canUse(player: PlayerEntity?) = true
 
-                    override fun onTakeOutput(player: PlayerEntity, stack: ItemStack) {
-                        super.onTakeOutput(player, stack)
-                        val newName = stack.name.string.trim()
-                        reopenCommunityCreation(player as ServerPlayerEntity, newName, currentShape, isManor)
+                    override fun onSlotClick(slotIndex: Int, button: Int, actionType: SlotActionType, player: PlayerEntity) {
+                        super.onSlotClick(slotIndex, button, actionType, player)
+
+                        if (slotIndex == OUTPUT_ID) {
+                            val outputStack = this.slots[OUTPUT_ID].stack
+                            val newName = outputStack.name.string.trim()
+                            reopenCommunityCreation(player as ServerPlayerEntity, newName, currentShape, isManor)
+                        }
                     }
 
                     override fun onClosed(player: PlayerEntity) {
--- src/main/kotlin/com/imyvm/community/inter/screen/component/ReadOnlySlot.kt
+++ src/main/kotlin/com/imyvm/community/inter/screen/component/ReadOnlySlot.kt
@@ -1,11 +1,11 @@
 package com.imyvm.community.inter.screen.component
 
 import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.inventory.SimpleInventory
+import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
 import net.minecraft.screen.slot.Slot
 
-class ReadOnlySlot(inv: SimpleInventory, index: Int, x: Int, y: Int) : Slot(inv, index, x, y) {
+class ReadOnlySlot(inv: Inventory, index: Int, x: Int, y: Int) : Slot(inv, index, x, y) {
     override fun canTakeItems(player: PlayerEntity) = false
     override fun canInsert(stack: ItemStack) = false
 }
