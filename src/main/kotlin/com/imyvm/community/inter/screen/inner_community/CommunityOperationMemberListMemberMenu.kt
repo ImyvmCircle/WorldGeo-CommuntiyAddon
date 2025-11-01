76c0ac97875d6e4277d844abf71267eed9faeceb
Doohaey Re <doohaey@gmail.com>
2025-11-01 14:22:11 +0800

    feat: content of community member management menu.

--- src/main/kotlin/com/imyvm/community/inter/screen/inner_community/CommunityOperationMemberListMemberMenu.kt
+++ src/main/kotlin/com/imyvm/community/inter/screen/inner_community/CommunityOperationMemberListMemberMenu.kt
@@ -5,47 +5,71 @@ import com.imyvm.community.domain.community.CommunityRole
 import com.imyvm.community.inter.screen.AbstractMenu
 import com.imyvm.community.inter.screen.component.createPlayerHeadItem
 import com.imyvm.community.util.Translator
+import net.minecraft.item.Items
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
 
 class CommunityOperationMemberListMemberMenu(
     syncId: Int,
-    community: Community,
-    playerObject: ServerPlayerEntity,
+    val community: Community,
+    private val playerObject: ServerPlayerEntity,
     playerExecutor: ServerPlayerEntity
 ) : AbstractMenu(
     syncId,
     menuTitle = generateCommunityMemberListMemberMenuTitle(community, playerObject)
-){
+) {
 
     init {
+        addDescriptionButtons()
+
+        val communityRole = community.getMemberRole(playerExecutor.uuid)
+        if (communityRole == CommunityRole.OWNER || communityRole == CommunityRole.ADMIN) addManageButtons()
+    }
+
+    private fun addDescriptionButtons() {
         addButton(
             slot = 10,
             name = playerObject.name.string,
             itemStack = createPlayerHeadItem(playerObject.name.string, playerObject.uuid)
         ) {}
+    }
 
-        addDescriptionButtons()
+    private fun addManageButtons() {
+        addButton(
+            slot = 12,
+            name = Translator.tr("ui.community.operation.member.member_page.button.region")?.string ?: "Region Setting",
+            item = Items.MAP
+        ) {}
 
-        val communityRole = community.getMemberRole(playerExecutor.uuid)
-        if (communityRole == CommunityRole.OWNER || communityRole == CommunityRole.ADMIN) {
-            addManageButtons()
+        addButton(
+            slot = 14,
+            name = Translator.tr("ui.community.operation.member.member_page.button.remove")?.string ?: "Remove Member",
+            item = Items.ZOMBIE_VILLAGER_SPAWN_EGG
+        ) {}
+
+        addButton(
+            slot = 19,
+            name = Translator.tr("ui.community.operation.member.member_page.button.message")?.string ?: "Send Message",
+            item = Items.PAPER
+        ) {}
+
+        if (community.getMemberRole(playerObject.uuid) == CommunityRole.OWNER) {
+            addButton(
+                slot = 21,
+                name = Translator.tr("ui.community.operation.member.member_page.button.promote")?.string ?: "Promote",
+                item = Items.COMMAND_BLOCK
+            ) {}
         }
-    }
-
-    private fun addDescriptionButtons(){
-
-    }
-
-    private fun addManageButtons(){
 
     }
 
     companion object {
         fun generateCommunityMemberListMemberMenuTitle(community: Community, playerObject: ServerPlayerEntity): Text {
-            return Text.of("${community.getRegion()?.name}" +
-                    " - ${playerObject.name.string}" +
-                    Translator.tr("ui.community.operation.member.member_page.title"))
+            return Text.of(
+                "${community.getRegion()?.name}" +
+                        " - ${playerObject.name.string}" +
+                        Translator.tr("ui.community.operation.member.member_page.title")
+            )
         }
     }
 }
