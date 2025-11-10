0e7469cec122759540454eb55a018558bdf26f8b
Doohaey Re <doohaey@gmail.com>
2025-11-10 23:44:26 +0800

    feat: add framework for community operation audit menu

--- src/main/kotlin/com/imyvm/community/inter/screen/inner_community/CommunityOperationAuditMenu.kt
+++ src/main/kotlin/com/imyvm/community/inter/screen/inner_community/CommunityOperationAuditMenu.kt
@@ -2,6 +2,8 @@ package com.imyvm.community.inter.screen.inner_community
 
 import com.imyvm.community.domain.Community
 import com.imyvm.community.inter.screen.AbstractMenu
+import com.imyvm.community.util.Translator
+import net.minecraft.item.Items
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
 
@@ -13,6 +15,23 @@ class CommunityOperationAuditMenu(
     syncId,
     menuTitle = generateMenuTitle(community)
 ) {
+    init {
+        val applicants = community.member.entries.filter { it.value.basicRoleType.name == "APPLICANT" }
+        if (applicants.isEmpty()) {
+            addButton(
+                slot = 10,
+                name = Translator.tr("ui.community.operation.audit.no_requests")?.string ?: "No Audit Requests",
+                item = Items.DARK_OAK_SIGN
+            ) {}
+        } else {
+            addApplicantButtons()
+        }
+    }
+
+    private fun addApplicantButtons() {
+
+    }
+
     companion object {
         fun generateMenuTitle(community: Community): Text = Text.of(community.generateCommunityMark() + " - Audit Requests:")
     }
