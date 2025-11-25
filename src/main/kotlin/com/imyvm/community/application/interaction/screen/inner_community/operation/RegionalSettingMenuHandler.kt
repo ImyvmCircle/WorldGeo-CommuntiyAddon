3c0e91d5fdf5b898937fd9e72870929631f06ef3
Doohaey Re <doohaey@gmail.com>
2025-11-25 23:49:52 +0800

    feat: togglePermissionSettingInRegion implement

--- src/main/kotlin/com/imyvm/community/application/interaction/screen/inner_community/operation/RegionalSettingMenuHandler.kt
+++ src/main/kotlin/com/imyvm/community/application/interaction/screen/inner_community/operation/RegionalSettingMenuHandler.kt
@@ -3,6 +3,9 @@ package com.imyvm.community.application.interaction.screen.inner_community.opera
 import com.imyvm.community.domain.Community
 import com.imyvm.iwg.domain.component.GeoScope
 import com.imyvm.iwg.domain.component.PermissionKey
+import com.imyvm.iwg.domain.component.SettingTypes
+import com.imyvm.iwg.inter.api.PlayerInteractionApi
+import com.imyvm.iwg.inter.api.RegionDataApi
 import com.mojang.authlib.GameProfile
 import net.minecraft.server.network.ServerPlayerEntity
 
@@ -13,17 +16,53 @@ fun togglePermissionSetting(
     playerObject: GameProfile?,
     permissionKey: PermissionKey
 ) {
-    togglePermissionSettingInRegion(community, scope, playerObject, permissionKey)
+    togglePermissionSettingInRegion(playerExecutor, community, scope, playerObject, permissionKey)
     refreshSettingInMenu(playerExecutor, community)
 }
 
 private fun togglePermissionSettingInRegion(
+    playerExecutor: ServerPlayerEntity,
     community: Community,
     scope: GeoScope?,
     playerObject: GameProfile?,
     permissionKey: PermissionKey
 ){
-
+    val region = community.getRegion() ?: return
+    if (scope == null) {
+        val currentPermissionSet = RegionDataApi.getRegionGlobalSettingsByType(region, SettingTypes.PERMISSION)
+        for (settingItem in currentPermissionSet) {
+            if ((settingItem.key == permissionKey) && ((!settingItem.isPersonal && (playerObject == null)) || ((playerObject != null) && (settingItem.playerUUID == playerObject.id)))) {
+                val newValue = settingItem.value == true
+                PlayerInteractionApi.removeSettingRegion(playerExecutor, region, permissionKey.toString(),
+                    playerObject?.id?.toString()
+                )
+                PlayerInteractionApi.addSettingRegion(playerExecutor, region, permissionKey.toString(),
+                    newValue.toString(), playerObject?.id?.toString()
+                )
+            } else {
+                PlayerInteractionApi.addSettingRegion(playerExecutor, region, permissionKey.toString(),
+                    false.toString(), playerObject?.id?.toString()
+                )
+            }
+        }
+    } else {
+        val currentPermissionSet = RegionDataApi.getScopeGlobalSettingsByType(scope, SettingTypes.PERMISSION)
+        for (settingItem in currentPermissionSet) {
+            if ((settingItem.key == permissionKey) && ((!settingItem.isPersonal && (playerObject == null)) || ((playerObject != null) && (settingItem.playerUUID == playerObject.id)))) {
+                val newValue = settingItem.value == true
+                PlayerInteractionApi.removeSettingScope(playerExecutor, region, scope.scopeName, permissionKey.toString(),
+                    playerObject?.id?.toString()
+                )
+                PlayerInteractionApi.addSettingScope(playerExecutor, region, scope.scopeName, permissionKey.toString(),
+                    newValue.toString(), playerObject?.id?.toString()
+                )
+            } else {
+                PlayerInteractionApi.addSettingScope(playerExecutor, region, scope.scopeName, permissionKey.toString(),
+                    false.toString(), playerObject?.id?.toString()
+                )
+            }
+        }
+    }
 }
 
 private fun refreshSettingInMenu(
