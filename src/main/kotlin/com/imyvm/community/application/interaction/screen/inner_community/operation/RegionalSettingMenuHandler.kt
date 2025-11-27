2c2e23efb6d64f3fb06be3a6ff1ab5d22cb0c79e
Doohaey Re <doohaey@gmail.com>
2025-11-27 23:06:48 +0800

    refactor: extract functions from togglePermissionSettingInRegion

--- src/main/kotlin/com/imyvm/community/application/interaction/screen/inner_community/operation/RegionalSettingMenuHandler.kt
+++ src/main/kotlin/com/imyvm/community/application/interaction/screen/inner_community/operation/RegionalSettingMenuHandler.kt
@@ -3,15 +3,17 @@ package com.imyvm.community.application.interaction.screen.inner_community.opera
 import com.imyvm.community.application.interaction.screen.CommunityMenuOpener
 import com.imyvm.community.domain.Community
 import com.imyvm.community.inter.screen.inner_community.operation.RegionalSettingMenu
+import com.imyvm.iwg.domain.Region
 import com.imyvm.iwg.domain.component.GeoScope
 import com.imyvm.iwg.domain.component.PermissionKey
 import com.imyvm.iwg.domain.component.SettingTypes
 import com.imyvm.iwg.inter.api.PlayerInteractionApi
 import com.imyvm.iwg.inter.api.RegionDataApi
 import com.mojang.authlib.GameProfile
-import net.minecraft.item.ItemStack
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
+import java.util.*
 
 fun getPermissionButtonItemStack(
     item: Item,
@@ -38,44 +40,24 @@ private fun togglePermissionSettingInRegion(
     playerExecutor: ServerPlayerEntity,
     community: Community,
     scope: GeoScope?,
-    playerObject: GameProfile?,
+    targetPlayer: GameProfile?,
     permissionKey: PermissionKey
 ) {
     val region = community.getRegion() ?: return
+    val targetPlayerId = targetPlayer?.id
 
-    val targetPlayerId = playerObject?.id
-    val targetPlayerIdStr = targetPlayerId?.toString()
+    val newValueStr = getNewPermissionValue(region, scope, targetPlayerId, permissionKey) ?: "false" // Temporary
+
     val permissionKeyStr = permissionKey.toString()
-
-    val currentPermissionSet = if (scope == null) {
-        RegionDataApi.getRegionGlobalSettingsByType(region, SettingTypes.PERMISSION)
-    } else {
-        RegionDataApi.getScopeGlobalSettingsByType(scope, SettingTypes.PERMISSION)
-    }
-
-    val existingSetting = currentPermissionSet.find { setting ->
-        val isKeyMatch = setting.key == permissionKey
-        val isPlayerMatch = (setting.playerUUID == targetPlayerId)
-        isKeyMatch && isPlayerMatch
-    }
-
-    val currentValue = existingSetting?.value == true
-    val newValueStr = (!currentValue).toString()
+    val targetPlayerIdStr = targetPlayerId?.toString()
 
     if (scope == null) {
-        PlayerInteractionApi.removeSettingRegion(
-            playerExecutor, region, permissionKeyStr, targetPlayerIdStr
-        )
-        PlayerInteractionApi.addSettingRegion(
+        setRegionSetting(
             playerExecutor, region, permissionKeyStr, newValueStr, targetPlayerIdStr
         )
     } else {
-        val scopeName = scope.scopeName
-        PlayerInteractionApi.removeSettingScope(
-            playerExecutor, region, scopeName, permissionKeyStr, targetPlayerIdStr
-        )
-        PlayerInteractionApi.addSettingScope(
-            playerExecutor, region, scopeName, permissionKeyStr, newValueStr, targetPlayerIdStr
+        setScopeSetting(
+            playerExecutor, region, scope.scopeName, permissionKeyStr, newValueStr, targetPlayerIdStr
         )
     }
 }
@@ -89,4 +71,54 @@ private fun refreshSettingInMenu(
     CommunityMenuOpener.open(playerExecutor) { syncId ->
         RegionalSettingMenu(syncId, playerExecutor, community, scope, playerProfile)
     }
+}
+
+private fun getNewPermissionValue(
+    region: Region,
+    scope: GeoScope?,
+    targetPlayerId: UUID?,
+    permissionKey: PermissionKey
+): String? {
+    val currentPermissionSet = if (scope == null) {
+        RegionDataApi.getRegionGlobalSettingsByType(region, SettingTypes.PERMISSION)
+    } else {
+        RegionDataApi.getScopeGlobalSettingsByType(scope, SettingTypes.PERMISSION)
+    }
+
+    val existingSetting = currentPermissionSet.find { setting ->
+        setting.key == permissionKey && setting.playerUUID == targetPlayerId
+    }
+
+    return existingSetting?.value?.toString()?.lowercase()
+}
+
+private fun setRegionSetting(
+    playerExecutor: ServerPlayerEntity,
+    region: Region,
+    permissionKeyStr: String,
+    newValueStr: String,
+    targetPlayerIdStr: String?
+) {
+    PlayerInteractionApi.removeSettingRegion(
+        playerExecutor, region, permissionKeyStr, targetPlayerIdStr
+    )
+    PlayerInteractionApi.addSettingRegion(
+        playerExecutor, region, permissionKeyStr, newValueStr, targetPlayerIdStr
+    )
+}
+
+private fun setScopeSetting(
+    playerExecutor: ServerPlayerEntity,
+    region: Region,
+    scopeName: String,
+    permissionKeyStr: String,
+    newValueStr: String,
+    targetPlayerIdStr: String?
+) {
+    PlayerInteractionApi.removeSettingScope(
+        playerExecutor, region, scopeName, permissionKeyStr, targetPlayerIdStr
+    )
+    PlayerInteractionApi.addSettingScope(
+        playerExecutor, region, scopeName, permissionKeyStr, newValueStr, targetPlayerIdStr
+    )
 }
