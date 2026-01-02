68295b0bcd45e0ba59377e861713a57a5e96bff3
Doohaey Re <doohaey@gmail.com>
2026-01-03 00:37:20 +0800

    feat: init confirmation menu

---
+++ src/main/kotlin/com/imyvm/community/inter/screen/ConfirmMenu.kt
@@ -0,0 +1,55 @@
+package com.imyvm.community.inter.screen
+
+import com.imyvm.community.util.Translator
+import net.minecraft.item.Items
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.Text
+
+class ConfirmMenu(
+    syncId: Int,
+    playerExecutor: ServerPlayerEntity,
+    private val cautions: List<String>,
+    private val runExecutor: (ServerPlayerEntity) -> Unit,
+    val runBack: (ServerPlayerEntity) -> Unit
+): AbstractMenu(
+    syncId = syncId,
+    menuTitle = getConfirmMenuTitle(
+        cautions.firstOrNull()
+        ?: Translator.tr("ui.confirm.default")?.string ?: "<>"),
+    runBack = runBack
+) {
+
+    init {
+        addCautionTextButtons()
+        addConfirmButton()
+    }
+
+    private fun addCautionTextButtons() {
+        if (cautions.size >= 5){
+            cautions.subList(0,5).forEachIndexed { index, string ->
+                addButton(
+                    slot = 1 + index * 9,
+                    name = string,
+                    item = Items.REDSTONE_TORCH
+                ) {}
+            }
+        }
+    }
+
+    private fun addConfirmButton() {
+        addButton(
+            slot = 35,
+            name = Translator.tr("ui.confirm.button.confirm")?.string ?: "Confirm",
+            item = Items.GREEN_WOOL
+        ) {
+            runExecutor
+        }
+    }
+
+    companion object {
+        private fun getConfirmMenuTitle(cautionTitle: String): Text {
+            return Translator.tr("ui.confirm.title", cautionTitle)
+                ?: Text.of("Confirm: $cautionTitle")
+        }
+    }
+}
